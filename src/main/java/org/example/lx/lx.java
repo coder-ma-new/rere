ff4d460ba4edc446dcab05e8734877f31ec8ee6e
coder-ma-new <mrc1326204901@sina.com>
2020-08-21 14:41:33 +0800

    xiangmu1

---
+++ src/main/java/org/example/lx/lx.java
@@ -0,0 +1,11 @@
+package org.example.lx;
+
+import com.mogujie.app1.util.ToolUtil;
+
+public class lx {
+
+    public static void main(String[] args) {
+        ToolUtil toolUtil=new ToolUtil();
+        toolUtil.sayHI();
+    }
+}
